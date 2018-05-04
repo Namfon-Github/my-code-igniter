3c2fe7261bce6395e979f404dfdcf00d0006182e
Namfon <nofman@aca80a80.ipt.aol.com>
2018-05-04 14:10:54 +0700

    จัดบน layout บนหน้าจอ responsive ใหม่

--- application/views/fishing-vessel/new-ship.php
+++ application/views/fishing-vessel/new-ship.php
@@ -1,16 +1,19 @@
 <div class="row">
-    <div class="col-4"></div>    
-    <div class="card col-4">
+    <div class="col-lg-3 col-md-1 col-sm-0"></div>    
+    <div class="card col-lg-6 col-md-10 col-sm-12">
         <div  class="card-body">
             <h5 class="card-title">ข้อมูลเรือประมง</h5>
         <form action="">
-            <label for="">Name</label>
-            <input type="text">
-            <button type="button" class="btn btn-primary">ADD</button>
-            <button type="button" class="btn btn-secondary">Cancel</button>
+            <div class=form-group">
+            <label for="">ชื่อ</label>
+            <input class="form-control" type="text">
+</div>
+            <button class="btn btn-primary btn-lg">เพิ่ม</button>
+            <button class="btn btn-secondary btn-lg">ล้าง</button>
             
 
         </form>
 </div>
     </div>
+    <div class="col-lg-3 col-md-1 col-sm-0"></div> 
 </div>
