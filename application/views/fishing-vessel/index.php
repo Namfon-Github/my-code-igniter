13a6cf181993842587d0e704efcc14abda10a1c0
Namfon <nofman@aca80a80.ipt.aol.com>
2018-05-04 14:10:21 +0700

    เอาข้อมูลจำลองออกและวนลูปแสดงข้อมูลเรือประมงจริง

--- application/views/fishing-vessel/index.php
+++ application/views/fishing-vessel/index.php
@@ -15,53 +15,27 @@
     <h1 class="col-lg-4 col-sm-12 col-md-10">Hello, Fisherman.</h1>
     <div class="col-lg-4 col-sm-12 col-md-1" style="background-color:green"></div>
 -->
+<?php
+    foreach($vessels as $ship): //วันตัวแรกใน array  $data['vessels'] = $result; จนถึงตัวสุดท้านใน array
 
+?>
     <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
         <div class="card">
             <div class="card-header">
-                ชื่อเรือ
+                <?php echo $ship['Name'] ?>
             </div>
             <div class="card-body">
-                <h5 class="card-title">ประเทศ</h5>
+                <h5 class="card-title"> 
+                    <?php echo $ship['Country_ID'] ?>
+                </h5>
                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
             </div>
         </div>
     </div>
+<?php
+ endforeach 
 
-    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-        <div class="card">
-            <div class="card-header">
-                ชื่อเรือ
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">ประเทศ</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-        </div>
-    </div>
+?>
 
-    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-        <div class="card">
-            <div class="card-header">
-                ชื่อเรือ
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">ประเทศ</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-        <div class="card">
-            <div class="card-header">
-                ชื่อเรือ
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">ประเทศ</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-        </div>
-    </div>
 
 </div>
