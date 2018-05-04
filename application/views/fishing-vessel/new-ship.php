16c8b182cf8e3cb815b92e45716fe207164c1807
Namfon <nofman@aca80a80.ipt.aol.com>
2018-05-04 14:23:38 +0700

    สร้าง form dropdowm list สำหรับข้อมูลประเทศ

--- application/views/fishing-vessel/new-ship.php
+++ application/views/fishing-vessel/new-ship.php
@@ -3,17 +3,22 @@
     <div class="card col-lg-6 col-md-10 col-sm-12">
         <div  class="card-body">
             <h5 class="card-title">ข้อมูลเรือประมง</h5>
-        <form action="">
-            <div class=form-group">
-            <label for="">ชื่อ</label>
-            <input class="form-control" type="text">
-</div>
-            <button class="btn btn-primary btn-lg">เพิ่ม</button>
-            <button class="btn btn-secondary btn-lg">ล้าง</button>
-            
-
-        </form>
-</div>
+        
+            <form action="">
+                <div class="form-group">
+                    <label for="">ชื่อ</label>
+                    <input class="form-control" type="text">
+                </div>
+                <div class="form-group">
+                    <label for="">ประเทศ</label>
+                    <select class="form-control">
+                        <option>Default select</option>
+                    </select>
+                </div>
+                <button class="btn btn-primary btn-lg">เพิ่ม</button>
+                <button class="btn btn-secondary btn-lg">ล้าง</button>
+            </form>
+        </div>
     </div>
     <div class="col-lg-3 col-md-1 col-sm-0"></div> 
 </div>
