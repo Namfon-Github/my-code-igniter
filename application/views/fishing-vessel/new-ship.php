6f89c71c40134ef2bdaa7decc6a42377ca1ce762
Namfon <nofman@aca80a80.ipt.aol.com>
2018-05-04 09:54:13 +0700

    ใส่ login form และการจัดวางตำแหน่ง

---
+++ application/views/fishing-vessel/new-ship.php
@@ -0,0 +1,16 @@
+<div class="row">
+    <div class="col-4"></div>    
+    <div class="card col-4">
+        <div  class="card-body">
+            <h5 class="card-title">ข้อมูลเรือประมง</h5>
+        <form action="">
+            <label for="">Name</label>
+            <input type="text">
+            <button type="button" class="btn btn-primary">ADD</button>
+            <button type="button" class="btn btn-secondary">Cancel</button>
+            
+
+        </form>
+</div>
+    </div>
+</div>
