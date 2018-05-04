7addc1d53ad5a52bc7725de388c13bbe653ef527
Namfon <nofman@aca80a80.ipt.aol.com>
2018-05-04 09:53:01 +0700

    ลองใส่ class สำหรับแบ่งคอลัมภ์ในการแสดงผลในแต่ละอุปกรณ์

---
+++ application/views/fishing-vessel/index.php
@@ -0,0 +1,18 @@
+<div class="row">
+   <!--
+    <div class="col-4"></div>
+    <h1 class="col-4">Hello, Fisherman.</h1>
+    <div class="col-4"></div>
+-->
+<!-- คือการใช้คำสั่งสำหรับแบ่งหน้าขนาดเล็ก ให้มี 12 คอลัมภ์-->
+<!--
+    <div class="col-lg-4 col-sm-12" style="background-color:blue"></div>
+    <h1 class="col-lg-4 col-sm-12">Hello, Fisherman.</h1>
+    <div class="col-lg-4 col-sm-12" style="background-color:green"></div>
+    -->
+<!--  -->
+    <div class="col-lg-4 col-sm-12 col-md-1" style="background-color:blue"></div>
+    <h1 class="col-lg-4 col-sm-12 col-md-10">Hello, Fisherman.</h1>
+    <div class="col-lg-4 col-sm-12 col-md-1" style="background-color:green"></div>
+
+</div>
