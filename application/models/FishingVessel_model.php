ffe79ad3930697a4d7b04e866286f22d5391b7e2
Namfon <nofman@aca80a67.ipt.aol.com>
2018-05-07 14:22:50 +0700

    รับค่าตัวแปร url ของ image file และ insert ลงฐานข้อมูล

--- application/models/FishingVessel_model.php
+++ application/models/FishingVessel_model.php
@@ -21,12 +21,12 @@ class FishingVessel_model extends CI_Model {
         return $result;
     }
 
-    public function save_new_vessel()
+    public function save_new_vessel($image_path)
     {
         
         $data['Name'] = $this->input->post('vesselName'); //$_POST['vesselName']
         $data['Country_ID'] = $this->input->post('country'); //$_POST['country']
-
+        $data['imagePath'] = $image_path;
         //var_dump($data);
         //INSERT INTO Vessel() VALUED ();
        return $this->db->insert('Vessel',$data);
