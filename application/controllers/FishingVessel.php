994d3b8912e554fd5b613bc249aae2cec3696b1f
Namfon <nofman@aca80a67.ipt.aol.com>
2018-05-07 14:20:30 +0700

    เพิ่ม image path เข้าไปตอนบันทึกข้อมูลเรือใหม่

--- application/controllers/FishingVessel.php
+++ application/controllers/FishingVessel.php
@@ -39,7 +39,12 @@ class FishingVessel extends CI_Controller
     {
         
         // เด๋วจะใส่ code uploadfile
+        $config['upload_path'] = './upload/';
+        $config['allowed_types'] = 'gif|jpg|png|jpeg';
+        //$config['maxsize'] = 100;
+        //$config['max_width'] =1024;
 
+        $this->load->library('upload', $config); //upload คือชื่อ library
         
         
         // การใช้ library เพื่อตรวจสอบความถูกต้องขอข้อมูล
@@ -61,10 +66,35 @@ class FishingVessel extends CI_Controller
         } else {
             // เป็นคำสั่งเพื่อดูว่าข้อมูลได้ถูกส่งมารึเปล่า
             // var_dump($_POST);
-            $this->load->model('fishingvessel_model');
-            $this->fishingvessel_model->save_new_vessel();
 
-            redirect('fishingvessel/');
+            $upload_success = $this->upload->do_upload('vesselImage');
+
+            if($upload_success)
+            {
+                $upload_data = $this->upload->data();
+                //var_dump($upload_data);
+
+                $image_path = 'upload/' . $upload_data['file_name'];
+
+                /*
+                $this->load->model('fishingvessel_model');
+                $this->fishingvessel_model->save_new_vessel();
+
+                redirect('fishingvessel/');
+            */
+            }
+            else{
+                $this->load->model('country_model');
+                $result = $this->country_model->get_all();
+                $data['country_list'] = $result;
+                $data['title'] = "เพิ่มข้อมูลเรือประมง";
+
+                $this->load->view('header', $data);
+                $this->load->view('fishing-vessel/new-ship', $data);
+                $this->load->view('footer');
+            }
+
+            
         }
     }
 
