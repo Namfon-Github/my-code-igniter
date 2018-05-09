64b24d96619cf052f149b5b4f1e9a257584e2dbd
Namfon <nofman@aca80a56.ipt.aol.com>
2018-05-09 15:45:26 +0700

    เพิ่ม web api สำหรับแสดงข้อมูลเรือทุกลำเป็น JSON

---
+++ application/controllers/api/fishingvessel.php
@@ -0,0 +1,21 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class FishingVessel extends CI_Controller
+{
+    public function index()
+    {
+        echo 'json';
+    }
+
+    public function all_ship()
+    {
+        $this->load->model('fishingvessel_model');
+        $data['vessels'] = $this->fishingvessel_model->get_all();
+        //var_dump($data);
+
+        echo json_encode($data['vessels']);
+    }
+}
+?>
